8f29388e4cdce85ce39b1419366f9b1d7bec7cb4
Agustín Viera <j.agustin.viera@gmail.com>
2019-02-27 23:36:13 -0300

    +cambios en login

--- ProyectoFinalPRO2018/UI/Logueo.aspx.cs
+++ ProyectoFinalPRO2018/UI/Logueo.aspx.cs
@@ -4,11 +4,44 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Negocio;
+using EntidadesCompartidas;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+            Limpiar();
+    }
 
+    private void Limpiar()
+    {
+        txtUsername.Text = string.Empty;
+        txtPassword.Text = string.Empty;
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Usuario u = negUsuario.Login(txtUsername.Text, txtPassword.Text);
+            if(u is Cliente)
+            {
+                Response.Redirect(Page.ResolveUrl("~/RealizarPedido.aspx"));
+            }
+            else if(u is Empleado)
+            {
+                Response.Redirect(Page.ResolveUrl("~/BienvenidaEmpleado.aspx"));
+            }
+            else
+            {
+                Limpiar();
+            }
+        }
+        catch (Exception ex)
+        {
+            lblError.Text = ex.Message;
+        }
     }
 }
